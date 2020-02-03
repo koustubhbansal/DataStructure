1915faf4ce86eb90d8f6ccb1eabf6bdf8b89d6ce
Koustubh Bansal <koustubhbansal10@gmail.com>
2020-02-03 22:29:11 +0530

    Height of a Tree

---
+++ HeightOfTree.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+int height=0;
+class Node
+{
+    public:
+    int data;
+    Node *left, *right;
+};
+
+Node* getNode(int data)
+{
+    Node* newNode = new Node;
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    return newNode;
+}
+int heightoftree(Node *root)
+{
+    if(root == NULL)
+    {
+        return 0;
+    }
+    height = 1 + ((heightoftree(root->left)>heightoftree(root->right))?heightoftree(root->left):heightoftree(root->right));
+
+}
+int main()
+{
+    Node* root = getNode(0);
+    root->left = getNode(1);
+    root->left->left = getNode(3);
+    root->left->left->left = getNode(7);
+    root->left->right = getNode(4);
+    root->left->right->left = getNode(8);
+    root->left->right->right = getNode(9);
+    root->left->right->right = getNode(9);
+    root->right = getNode(2);
+    root->right->left = getNode(5);
+    root->right->right = getNode(6);
+
+    heightoftree(root);
+
+    cout<<height;
+}
